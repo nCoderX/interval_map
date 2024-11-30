81cd4038ebe3110b9e3e8128b60c3a9f88d07156
Fernando Moreira <fernando.moreira@outlook.com>
2024-11-30 13:46:32 +0000

    Improve interval_map readability and add new test case
    Updated interval_map.h for better readability and functionality:
    - Added includes for type traits and utility functionalities.
    - Reformatted concepts and class methods for clarity.
    - Introduced ForwardableTo concept.
    - Enhanced interval_map class with templated constructor and assign method.
    
    Added new test case in test.cpp:
    - TEST(intervals, sequence_double_to_string) to validate interval_map with double keys and string values.

--- ThinkCellTest/interval_map.h
+++ ThinkCellTest/interval_map.h
@@ -2,55 +2,68 @@
 
 #include <map>
 #include <concepts>
+#include <type_traits>
+#include <utility>
 
 template<typename T>
-concept IntevalMapKeyType = std::convertible_to<decltype(std::declval<T>() < std::declval<T>()), bool> && std::copyable<T > && std::is_move_constructible_v<T> && std::is_move_assignable_v<T>;
+concept IntevalMapKeyType = std::convertible_to<decltype(std::declval<T>() < std::declval<T>()), bool> 
+    && std::copyable<T> 
+    && std::is_move_constructible_v<T> 
+    && std::is_move_assignable_v<T>;
 
 template<typename T>
-concept IntevalMapValueType = std::convertible_to<decltype(std::declval<T>() == std::declval<T>()), bool> && std::copyable<T>&& std::is_move_constructible_v<T>&& std::is_move_assignable_v<T>;
+concept IntevalMapValueType = std::convertible_to<decltype(std::declval<T>() == std::declval<T>()), bool> 
+    && std::copyable<T> 
+    && std::is_move_constructible_v<T> 
+    && std::is_move_assignable_v<T>;
+
+template<typename T, typename U>
+concept ForwardableTo = std::is_constructible_v<U, T> && std::is_assignable_v<U&, T>;
 
 template<IntevalMapKeyType K, IntevalMapValueType V>
 class interval_map {
-	using interval_map_t = std::map<K, V>;
+    using interval_map_t = std::map<K, V>;
     V _valBegin;
-	interval_map_t _intervalsMap;
-	
+    interval_map_t _intervalsMap;
+    
 public:
-	interval_map(interval_map const& other) = default;
-	interval_map(interval_map&& other) noexcept
-		: _valBegin(std::move(other._valBegin)), _intervalsMap(std::move(other._intervalsMap)) {
-	}
-	interval_map(V&& val) noexcept
-		: _valBegin(std::forward<V>(val)) {
-	}
-	interval_map& operator=(interval_map const& other) = default;
-	interval_map& operator=(interval_map&& other) noexcept {
-		_valBegin = std::move(other._valBegin);
-		_intervalsMap = std::move(other._intervalsMap);
-		return *this;
-	}
+    interval_map(interval_map const& other) = default;
+    interval_map(interval_map&& other) noexcept
+        : _valBegin(std::move(other._valBegin)), _intervalsMap(std::move(other._intervalsMap)) {
+    }
+    template<ForwardableTo<V> T>
+    interval_map(T&& val) noexcept
+        : _valBegin(std::forward<T>(val)) {
+    }
+    interval_map& operator=(interval_map const& other) = default;
+    interval_map& operator=(interval_map&& other) noexcept {
+        _valBegin = std::move(other._valBegin);
+        _intervalsMap = std::move(other._intervalsMap);
+        return *this;
+    }
 
-	[[nodiscard]] typename interval_map_t::iterator begin() const noexcept {
-		return _intervalsMap.begin();
-	}
-	[[nodiscard]] typename interval_map_t::iterator end() const noexcept {
-		return _intervalsMap.end();
-	}
-	[[nodiscard]] const interval_map_t& intervals() const noexcept {
-		return _intervalsMap;
-	}
-	[[nodiscard]] const V& valBegin() const noexcept {
-		return _valBegin;
-	}
-	void swap(interval_map& other) noexcept {
-		std::swap(_valBegin, other._valBegin);
-		_intervalsMap.swap(other._intervalsMap);
-	}
-    void assign(K const& keyBegin, K const& keyEnd, V&& val) noexcept {
+    [[nodiscard]] typename interval_map_t::iterator begin() const noexcept {
+        return _intervalsMap.begin();
+    }
+    [[nodiscard]] typename interval_map_t::iterator end() const noexcept {
+        return _intervalsMap.end();
+    }
+    [[nodiscard]] const interval_map_t& intervals() const noexcept {
+        return _intervalsMap;
+    }
+    [[nodiscard]] const V& valBegin() const noexcept {
+        return _valBegin;
+    }
+    void swap(interval_map& other) noexcept {
+        std::swap(_valBegin, other._valBegin);
+        _intervalsMap.swap(other._intervalsMap);
+    }
+    template<ForwardableTo<V> T>
+    void assign(K const& keyBegin, K const& keyEnd, T&& val) noexcept {
         auto itValue = [this](const auto& it) -> const V& { return it == std::end(_intervalsMap) ? _valBegin : it->second; };
-		auto previousValue = [this](const auto& it) -> const V& { return it == std::begin(_intervalsMap) ? _valBegin : std::prev(it)->second; };
+        auto previousValue = [this](const auto& it) -> const V& { return it == std::begin(_intervalsMap) ? _valBegin : std::prev(it)->second; };
         if (!(keyBegin < keyEnd)) 
-	        return;
+            return;
         auto itOverlapStart = _intervalsMap.upper_bound(keyBegin);
         auto itPrevInterval = (itOverlapStart == std::begin(_intervalsMap)) ? std::end(_intervalsMap) : std::prev(itOverlapStart);
         auto extensionValue = itValue(itPrevInterval);
@@ -61,23 +74,23 @@ public:
             }
             return true;
         });
-		if (itOverlapStart != itNextInterval) 
+        if (itOverlapStart != itNextInterval) 
             itNextInterval = _intervalsMap.erase(itOverlapStart, itNextInterval);
         if (!(extensionValue == val) && (itNextInterval == std::end(_intervalsMap) || keyEnd < itNextInterval->first)) 
             itNextInterval = _intervalsMap.emplace(keyEnd, std::move(extensionValue)).first;
-		if (!(itValue(itPrevInterval) == val)) {
-			if (itPrevInterval != std::end(_intervalsMap) && !(itPrevInterval->first < keyBegin))
-				if (previousValue(itPrevInterval) == val)
-					_intervalsMap.erase(itPrevInterval);
-				else
-					itPrevInterval->second = std::forward<V>(val);
-			else
-				_intervalsMap.emplace(keyBegin, std::forward<V>(val));
-		}
+        if (!(itValue(itPrevInterval) == val)) {
+            if (itPrevInterval != std::end(_intervalsMap) && !(itPrevInterval->first < keyBegin))
+                if (previousValue(itPrevInterval) == val)
+                    _intervalsMap.erase(itPrevInterval);
+                else
+                    itPrevInterval->second = std::forward<T>(val);
+            else
+                _intervalsMap.emplace(keyBegin, std::forward<T>(val));
+        }
     }
 
-	V const& operator[](K const& key) const noexcept{
-		auto it = _intervalsMap.upper_bound(key);
-		return it == std::begin(_intervalsMap)? _valBegin : std::prev(it)->second;
-	}
+    V const& operator[](K const& key) const noexcept {
+        auto it = _intervalsMap.upper_bound(key);
+        return it == std::begin(_intervalsMap) ? _valBegin : std::prev(it)->second;
+    }
 };
